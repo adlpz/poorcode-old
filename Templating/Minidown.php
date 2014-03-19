518d47c0c1f15328bb64ba8a8fdcd45b862b6e00
Adrià López Lozano <adria@prealfa.com>
2014-03-19 02:45:40 +0100

    improved styles. improved handling of code blocks by minidown'

--- Templating/Minidown.php
+++ Templating/Minidown.php
@@ -12,19 +12,23 @@ class Markdown
 {
     public static function markdown($markdown)
     {
+        $codes = [];
         $rules = [
+            '/((?:`)+)(.*?)\1/s' => function ($m) use (&$codes) { $uid = uniqid('C0DE'); $codes[$uid] = $m[2]; return strlen($m[1]) > 1 ? "<pre><code>$uid</code></pre>" : "<code>$uid</code>"; },
             '/^(#+)\s*(.+)/m' => function($m) { $h = strlen($m[1]); return sprintf('<h%s>%s</h%s>', $h, $m[2], $h); },
             '/\!\[([^\]]+)\]\(([^\)]+)\)/' => '<img src="\2" title="\1">',
             '/\[([^\]]+)\]\(([^\)]+)\)/' => '<a href="\2">\1</a>',
             '/(\*\*|__)(.*?)\1/' => '<strong>\2</strong>',
             '/(\*|_)(.*?)\1/' => '<em>\2</em>',
-            '/((?:`)+)(.*?)\1/s' => function ($m) { $s = str_replace("\n", "\0", '<code>'.$m[2].'</code>'); return strlen($m[1]) > 1 ? "<pre>$s</pre>" : $s; },
         ];
         foreach ($rules as $regex => $sub) {
             $markdown = call_user_func((is_callable($sub) ? 'preg_replace_callback' : 'preg_replace'), $regex, $sub, $markdown);
         }
         $lines = array_filter(array_map('trim', explode("\n\n", $markdown)));
         $html = implode("\n", array_map(function($t){ return preg_match("/^\<(h|p|ul|li|blockquote|hr|br|pre).*/", $t) ? $t : "<p>$t</p>"; }, $lines));
-        return str_replace("\0", "\n", $html);
+        foreach ($codes as $uid => $code) {
+            $html = str_replace($uid, htmlentities($code), $html);
+        }
+        return $html;
     }
 }
